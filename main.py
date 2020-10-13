44b82160f36b1e5607d00501e1eb64fa94e09680
nanyssalazar <andrea.sosa01@iest.edu.mx>
2020-10-12 22:29:22 -0500

    Operaciones con conjuntos: unión e intersección. Falta hacer más testing.

--- main.py
+++ main.py
@@ -1,10 +1,120 @@
 from PyQt5 import QtWidgets
+from PySide2.QtCore import SIGNAL
 import UI
 
+remove = ["A", 'B', 'C', '=', '{', '}']
 
-def funcion():
-    print("hola")
+setA = set()
+setB = set()
+setC = set()
+set_conjAB = set()
+set_conjAC = set()
+set_conjABC = set()
 
+def funcion_conjA():
+    #ui.operacion.setText(ui.operacion.text() + ui.btn_conjA.text())
+    string = ui.conjA.text()
+    ui.operacion.setText(ui.operacion.text()+'A')
+    for i in remove:
+        if i in ui.operacion.text():
+            string = string.replace(i, '')
+    for i in string.split(','):
+        setA.add(i)
+
+def funcion_conjB():
+    if 'U' in ui.operacion.text():
+        string = ui.conjB.text()
+        ui.operacion.setText(ui.operacion.text() + 'B')
+        for i in remove:
+            if i in ui.operacion.text():
+                string = string.replace(i, '')
+        for i in string.split(','):
+            setB.add(i)
+        #set_conj1 = setA.union(setB)
+        #print(set_conj1)
+
+    # string = ui.conjB.text()
+    # ui.operacion.setText(ui.operacion.text()+'B')
+    # remove = ["A", 'B', 'C', '=', '{', '}']
+    # for i in remove:
+    #     if i in ui.operacion.text():
+    #         string = string.replace(i, '')
+    # for i in string.split(','):
+    #     setB.add(i)
+    # print(setB)
+
+def funcion_conjC():
+    string = ui.conjC.text()
+    ui.operacion.setText(ui.operacion.text() + 'C')
+    for i in remove:
+        if i in ui.operacion.text():
+            string = string.replace(i, '')
+    for i in string.split(','):
+        setC.add(i)
+    # if 'UC' in ui.operacion.text():
+    #     print(setA.union(setB).union(setC))
+    # if "AUB∩C" in ui.operacion.text():
+    #     print(setA.union(setB).intersection(setC))
+
+
+def funcion_union():
+    ui.operacion.setText(ui.operacion.text() + ui.btn_union.text())
+
+def funcion_inter():
+    ui.operacion.setText(ui.operacion.text() + '∩')
+
+def funcion_igual():
+    ui.operacion.setText(ui.operacion.text() + ui.btn_igual.text())
+    if "AUB" in ui.operacion.text():
+        set_conjAB = setA.union(setB)
+        print(set_conjAB)
+        ui.resultado.setPlainText("AUB = " + str(set_conjAB))
+
+    if "AUC" in ui.operacion.text():
+        set_conjAC = setA.union(setC)
+        print(set_conjAC)
+        ui.resultado.setPlainText("AUB = " + str(set_conjAC))
+
+    if "A∩B" in ui.operacion.text():
+        set_conjAB = setA.intersection(setB)
+        print(set_conjAB)
+        ui.resultado.setPlainText("AUB = " + str(set_conjAB))
+
+    if "A∩C" in ui.operacion.text():
+        set_conjAC = setA.intersection(setC)
+        print(set_conjAC)
+        ui.resultado.setPlainText("AUB = " + str(set_conjAC))
+
+    if "AUB∩C" in ui.operacion.text():
+        set_conjABC = (setA.union(setB)).intersection(setC)
+        print(set_conjABC)
+        ui.resultado.setPlainText("AUB = " + str(set_conjABC))
+
+    if "AUBUC" in ui.operacion.text():
+        set_conjABC = setA.union(setB).union(setC)
+        print(set_conjABC)
+        ui.resultado.setPlainText("AUB = " + str(set_conjABC))
+
+
+    # string = ui.operacion.text()
+    # remove = ["A", 'B', 'C', '=', '{', '}']
+    # for i in remove:
+    #     if i in ui.operacion.text():
+    #         string = string.replace(i, '')
+    # print(string.split(','))
+
+def funcion_ac():
+    setA.clear()
+    setB.clear()
+    setC.clear()
+    set_conjAB.clear()
+    set_conjAC.clear()
+    set_conjABC.clear()
+    ui.operacion.clear()
+    ui.conjA.clear()
+    ui.conjB.clear()
+    ui.conjC.clear()
+    ui.resultado.clear()
 
 if __name__ == "__main__":
     import sys
@@ -14,7 +124,14 @@ if __name__ == "__main__":
     ui.setupUi(main_window)
 
     # Aquí van las funciones
-    ui.btn_union.clicked.connect(funcion)
+    #ui.btn_union.clicked.connect(funcion)
+    ui.btn_conjA.clicked.connect(funcion_conjA)
+    ui.btn_conjB.clicked.connect(funcion_conjB)
+    ui.btn_union.clicked.connect(funcion_union)
+    ui.btn_igual.clicked.connect(funcion_igual)
+    ui.btn_conjC.clicked.connect(funcion_conjC)
+    ui.btn_interseccion.clicked.connect(funcion_inter)
+    ui.btn_ac.clicked.connect(funcion_ac)
 
     main_window.show()
     sys.exit(app.exec_())
